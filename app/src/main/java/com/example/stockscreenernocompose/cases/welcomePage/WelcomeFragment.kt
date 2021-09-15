0222d32302c63efd8503f1a9145b94dc28c4ebe1
Dimitris <dimitrisgkinis1990@gmail.com>
2021-09-15 14:14:43 +0300

    Welcome Screen UI implementation

--- app/src/main/java/com/example/stockscreenernocompose/cases/welcomePage/WelcomeFragment.kt
+++ app/src/main/java/com/example/stockscreenernocompose/cases/welcomePage/WelcomeFragment.kt
@@ -1,11 +1,11 @@
 package com.example.stockscreenernocompose.cases.welcomePage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.stockscreenernocompose.R
 import com.example.stockscreenernocompose.databinding.FragmentWelcomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +19,23 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentWelcomeBinding.bind(view)
+
+        initUI()
+    }
+
+    private fun initUI() {
+
+        binding.validateButton.setOnClickListener {
+            val validationResult =
+                viewModel.validateSymbol(binding.tickerSymbolEditText.text.toString())
+
+            if (validationResult == 0) {
+                val action = WelcomeFragmentDirections.actionWelcomeFragmentToResultFragment()
+                findNavController().navigate(action)
+            } else {
+                Toast.makeText(context, getString(validationResult), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/stockscreenernocompose/cases/welcomePage/WelcomeViewModel.kt
+++ app/src/main/java/com/example/stockscreenernocompose/cases/welcomePage/WelcomeViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.stockscreenernocompose.cases.welcomePage
 
 import androidx.lifecycle.ViewModel
+import com.example.stockscreenernocompose.R
 
-class WelcomeViewModel: ViewModel() {
+class WelcomeViewModel : ViewModel() {
+
+    fun validateSymbol(symbol: String): Int {
+        val trimmedSymbol = symbol.replace(" ", "")
+        return if (trimmedSymbol.isBlank()) {
+            R.string.app_ticker_symbol
+        } else 0
+    }
 }
